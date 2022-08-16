d33ad88177479d92ea28c85bc8934b39721bb98e
sean-code <john.nganga@student.moringaschool.com>
2022-08-16 08:37:32 +0300

    completed 3 labs

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-
 // Write your code here
+// Test 1
+let num1 = 62;
+let num2 = 1;
+const multiply = num1 * num2;
+
+// Test 2
+function random(){
+    return Math.random()+ 0.1;
+}
+random();
+
+
+// Test 3
+let num3 = 22; 
+let num4 = 9;
+const mod = num3 % num4;
+
+// Test 4
+const max = Math.max(2, 6, 8, 10, 20);
+
